9f52898bb3e37f4cb26cdc81d1ecceb2415c0a32
Sean <simpson.sean@outlook.com>
2021-09-09 16:53:00 -0700

    WORKDONE: Intial ACP; Folder and file structure created.

---
+++ src/components/app/App.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import NewsContainer from '../../containers/NewsContainer.jsx';
+
+export default function App() {
+  return <NewsContainer />;
+
+} // <--- END PARENT BLOCK
